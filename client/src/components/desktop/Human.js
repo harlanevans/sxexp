27815aabd0e7389e16f231bcadeb98f2e7836bb5
Harlan Evans <harlanevans5@gmail.com>
2019-12-04 20:01:18 -0700

    update human page, contact form to van@genhu.org

--- client/src/components/desktop/Human.js
+++ client/src/components/desktop/Human.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Title, TitleCont, Text, TextCont } from "../styles/Styles";
 import { Fade } from "react-reveal";
-import Child from "../../assets/images/Child2.jpeg";
+import Boys from "../../assets/images/MonteriaBoys.jpeg";
 import Footer from "./Footer";
 
 const Human = () => {
@@ -10,59 +10,69 @@ const Human = () => {
       <div className="comp-cont-pages">
         <Fade duration={2000} top>
           <TitleCont style={{ padding: "2em 0em 2em" }}>
-            <Title>Humanitarian Involvement</Title>
+            <Title>Humanitarian Work</Title>
           </TitleCont>
         </Fade>
+
         <Fade duration={2000} delay={500}>
-        <div className="row img-cont">
-          <img src={Child} className="human-img" alt='child'/>
-        </div>
+          <div className="row" style={{ padding: "2em 0em" }}>
+            <TextCont>
+              <Text>
+                Southern Cross tours and expeditions usually include a visit to
+                one of the homes operated by Generations Humanitarian, a
+                501(c)(3) nonprofit organization that builds and operates
+                orphanages and drop-in centers for street children in Latin
+                America. Each trip to Peru, Colombia, or Ecuador may include a
+                humanitarian element of taking down suitcases of socks and
+                underwear to poor, orphaned, refugee, or victims of armed
+                conflict children.
+                {/* Dr. Evans is also the founder of{" "}
+                <a
+                  href="https://genhu.org/"
+                  target="_blank"
+                  className="human-link"
+                  rel="noopener noreferrer"
+                >
+                  Generations Humanitarian
+                </a>{" "}
+                (2008). Generations Humanitiarian builds and operates orphanages
+                and drop-in centers for street children in Latin America. As of
+                June 2018, they have helped rear more than{" "}
+                <span className="human-stat">1494</span> children, feeding more
+                than 15,000 meals every month to children, for a total of{" "}
+                <span className="human-stat">735,822</span> meals to date. Your
+                Book of Mormon Interest tour may include a stop at one of our
+                orphanages just outside of Cusco, Peru. Each trip to Peru,
+                Colombia, or Ecuador includes a humanitairn element of taking
+                down suitcases of socks and underwear to poor, orphaned,
+                refugee, or victims of armed conflict children. */}
+              </Text>
+            </TextCont>
+          </div>
         </Fade>
         <Fade duration={2000} delay={500}>
-        <div className="row" style={{padding: "2em 0em"}}>
-          <TextCont>
-            <Text>
-              Dr. Evans is also the founder of{" "}
-              <a
-                href="https://genhu.org/"
-                target="_blank"
-                className="human-link"
+          <div className="row">
+            <TextCont>
+              <Text>
+                Visit the{" "}
+                <a
+                  href="https://genhu.org/"
+                  target="_blank"
+                  className="human-link"
                   rel="noopener noreferrer"
                 >
-                Generations Humanitarian
-              </a>{" "}
-              (2008). Generations Humanitiarian builds and operates orphanages
-              and drop-in centers for street children in Latin America. As of
-              June 2018, they have helped rear more than <span className='human-stat'>1494</span>{" "}
-              children, feeding more than 15,000 meals every month to children,
-              for a total of <span className='human-stat'>735,822</span> meals to date. Your Book of
-              Mormon Interest tour may include a stop at one of our orphanages
-              just outside of Cusco, Peru. Each trip to Peru, Colombia, or
-              Ecuador includes a humanitairn element of taking down suitcases of
-              socks and underwear to poor, orphaned, refugee, or victims of
-              armed conflict children.
-            </Text>
-          </TextCont>
-        </div>
-              </Fade>
+                  Generations Humanitarian
+                </a>{" "}
+                site and make a donation.
+              </Text>
+            </TextCont>
+          </div>
+        </Fade>
         <Fade duration={2000} delay={500}>
-        <div className="row">
-          <TextCont>
-            <Text>
-              Visit the{" "}
-              <a
-                href="https://genhu.org/"
-                target="_blank"
-                className="human-link"
-                  rel="noopener noreferrer"
-                >
-                Generations Humanitarian
-              </a>{" "}
-              site or make a donation.
-            </Text>
-          </TextCont>
-        </div>
-                </Fade>
+          <div className="row img-cont" style={{padding: "1em 0em 2em"}}>
+            <img src={Boys} className="human-img" alt="Boys" />
+          </div>
+        </Fade>
       </div>
       <Footer />
     </div>
